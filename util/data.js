4a70d9434b52fb033e51e553ad960a9ceaeda3ea
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2023-01-20 18:09:14 +0530

    data file

---
+++ util/data.js
@@ -0,0 +1,12 @@
+const items = [
+    {
+        name: 'Wheel bar 80g',
+        price: 5,
+        category: 'Soap',
+        image: 'https://m.media-amazon.com/images/I/81+rncuYHHL._SL1500_.jpg'
+    },
+    {
+        name: 'Nirma'
+    },
+
+]
