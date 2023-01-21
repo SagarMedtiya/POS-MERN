7120ccc318865928f16929e1ee3107d3807767c5
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2023-01-21 21:35:27 +0530

    database to add

--- util/data.js
+++ util/data.js
@@ -6,7 +6,24 @@ const items = [
         image: 'https://m.media-amazon.com/images/I/81+rncuYHHL._SL1500_.jpg'
     },
     {
-        name: 'Nirma'
+        name: 'Nirma',
+        price: 5,
+        category: 'soap',
+        image: 'https://m.media-amazon.com/images/I/81+rncuYHHL._SL1500_.jpg'
     },
+    {
+        name:'Parle G',
+        price: 5,
+        category: 'Biscuits',
+        image:'https://www.jiomart.com/images/product/original/491539619/parle-g-original-glucose-biscuits-100-g-product-images-o491539619-p491539619-0-202208121332.jpg'
+    },
+    {
+        name: 'Milk Bikies',
+        price: 10,
+        category:'Biscuits',
+        image: 'https://newassets.apollo247.com/pub/media/catalog/product/b/r/bri0137.jpg'
+    }
 
 ]
+
+module.exports = items;
