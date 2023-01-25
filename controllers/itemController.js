c56bc3ce0defc4605af12a85e5cb4c315e7327ef
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2023-01-25 22:09:39 +0530

    add additemcontroller function

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,10 +1,25 @@
 const itemModel =  require('../models/itemModel')
-export const getItemController = async (req, res)=>{
-    try{
-        const items = await itemModel 
-    }
-    catch(error){
-        console.log(error)
-    }
 
+class itemController{
+    async getItemController (req, res){
+        try{
+            const items = await itemModel.find(); 
+            res.status(200).send(items);
+        }
+        catch(error){
+            console.log(error);
+        } 
+    }
+    async addItemController(req,res){
+        try{
+            const newItem = new itemModel(req.body);
+            await newItem.save();
+            res.status(201).send("Itewm created successfully");
+        }
+        catch(error){
+            res.status(400).send("error",error);
+            console.log(error);
+        }
+    }
 }
+module.exports = new itemController();
