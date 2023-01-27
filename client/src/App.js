521ccc82845b5ae59e07694c1ca8d95b3dde280f
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2023-01-27 17:24:20 +0530

    add two routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
-
+import 'antd/dist/antd.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ItemPage from './pages/ItemPage';
 function App() {
+    const router = createBrowserRouter([
+        {
+            path: "/" ,
+            element: <HomePage/>
+        },
+        {
+            path: "items",
+            element : <ItemPage/>      
+        }
+    ])
   return (
-    <div className="App">
-      
+    <div >
+       <RouterProvider router={router} />
     </div>
   );
 }
