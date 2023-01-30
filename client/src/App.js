7425765fde39244b44f1051eb7c9ee14607a9ad3
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2023-01-30 14:37:31 +0530

    fix antd

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import 'antd/dist/antd.css';
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ItemPage from './pages/ItemPage';
