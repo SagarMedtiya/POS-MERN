e44c38775edbaf10ea6fb82fb87c8d9d975f912b
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2023-02-10 23:33:00 +0530

    create a store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,18 +1,5 @@
-import {createStore, combineReducers, applyMiddleWare} from 'redux';
-import thunk from 'redux-thunk';
-import {composeWithDevTools} from 'redux-devtools-extension';
-import { rootReducer } from './rootReducer';
+import { configureStore } from '@reduxjs/toolkit';
 
-const finalReducer = combineReducers({
-    rootReducer: rootReducer
-})
-
-const initialState = {
-    rootReducer:{
-        cartItems : localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : []
-    },
-};
-
-const middleware = [thunk]
-
-const store = createStore(rootReducer)
+export default configureStore({
+    reducer: {}
+  })
