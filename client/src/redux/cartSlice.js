8b2599546b704895473c371639a64d0e7feefef4
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2023-02-13 20:00:35 +0530

    create the reducers

--- client/src/redux/cartSlice.js
+++ client/src/redux/cartSlice.js
@@ -4,9 +4,16 @@ const initialState ={
     loading: false,
     cartItems: []
 }
-
-
 export const cartSlice = createSlice({
     name: 'cart',
     initialState,
+    reducers:{
+        setCart:(state, action)=>{
+            const { cartItems } = action.payload;
+            state.cartItems = cartItems;
+        }
+    }
 })
+
+export const { setCart} = authSlice.actions
+export default authSlice.reducer
