f8471e3c223eccef34f8a8b2ff3ff5013a87c01c
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2023-02-20 19:31:38 +0530

    remove hoverable from card component

--- client/src/components/ItemList.js
+++ client/src/components/ItemList.js
@@ -1,15 +1,18 @@
 import React from 'react';  
-import { Card } from 'antd';
+import { Card, Button } from 'antd';
 
 const ItemList = ({item}) => {
     const { Meta } = Card;
     return (
         <Card
-        hoverable
+        
         style={{  width: 200, marginBottom:10}}
         cover={<img alt={item.name} src={item.image} style={{height: 150}}/>}
         >
         <Meta title={item.name}  />
+        <div className='item-button'>
+            <Button>Add To Cart</Button>            
+        </div>
         </Card>    
     )
 }
