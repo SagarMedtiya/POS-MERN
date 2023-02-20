9c0c62b0c28822b3ead158c1d7ea7c827c6e0932
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2023-02-20 19:32:05 +0530

    add the cartSlice to the store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import cartSlice from './cartSlice';
 export default configureStore({
-    reducer: {}
+    reducer: {
+        cartSlice
+    }
   })
