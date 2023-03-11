8f06ce93687acfa84e5a0c30fbf4d185d6ac186c
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2023-03-11 23:58:00 +0530

    add the dispatch module

--- client/src/components/ItemList.js
+++ client/src/components/ItemList.js
@@ -1,18 +1,29 @@
 import React from 'react';  
 import { Card, Button } from 'antd';
-
+import {useSelector, useDispatch} from 'react-redux';
+import { setCart } from '../redux/cartSlice';
 const ItemList = ({item}) => {
+    //update the cart handler
+    const dispatch = useDispatch();
+    async function handleAddToCart(){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
     const { Meta } = Card;
     return (
         <Card
         
         style={{  width: 200, marginBottom:10}}
-        cover={<img alt={item.name} src={item.image} style={{height: 150}}/>}
-        >
+        cover={<img alt={item.name} src={item.image} style={{height: 150}}/>}>
+        
         <Meta title={item.name}  />
   
         <div className='item-button'>
-            <Button>Add To Cart</Button>            
+            <Button onClick={handleAddToCart}>Add To Cart</Button>            
         </div>
         </Card>    
     )
